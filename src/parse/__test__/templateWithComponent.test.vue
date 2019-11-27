99f3367fa0fd616dc96ab50eba68ecee3ebe3f0d
IWANABETHATGUY <974153916@qq.com>
2019-11-27 22:29:41 +0800

    test(test): test

---
+++ src/parse/__test__/templateWithComponent.test.vue
@@ -0,0 +1,22 @@
+<template>
+  <MediaItem
+    v-for="(item, index) in recommendMovies"
+    :key="index + 'b'"
+    :width="48"
+    :height="48"
+    :to="movieLink(item.media_id)"
+    @pageShow="
+      reportFilm('_show', {
+        film_id: item.media_id,
+        title: item.media_info.name,
+      })
+    "
+    @pageClick="
+      reportFilm('comk', {
+        film: item.m_id,
+        title: item.m_id.name,
+      })
+    "
+    :media="item"
+  />
+</template>
