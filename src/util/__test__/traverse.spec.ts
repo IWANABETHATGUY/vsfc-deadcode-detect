16916d07003d15aff33d693f2271b3dffa431065
IWANABETHATGUY <974153916@qq.com>
2019-11-28 13:13:41 +0800

    test(traverse): test

--- src/util/__test__/traverse.spec.ts
+++ src/util/__test__/traverse.spec.ts
@@ -1,6 +1,6 @@
 import { traverseTemplateAst } from '../traverse';
 describe('traverse TemplateAst', () => {
   test('traverse a element not a node', () => {
-    expect(traverseTemplateAst({}, [])).toEqual([]);
+    expect(traverseTemplateAst(null, [])).toEqual([]);
   });
 });
