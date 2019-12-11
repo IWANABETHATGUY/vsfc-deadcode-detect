b9c0d21a06ebb1302f39955eff196a52901980ea
IWANABETHATGUY <974153916@qq.com>
2019-12-11 22:42:45 +0800

    docs(readme): 已知的问题

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # vscf-deadcode-detect
 a repo to detect vue single file component dead code 
+
+
+## 已知的问题
+1. 在vue中这样写是合法的，但是对于一个js表达式是不合法，因此这样的情况，是无法从template中解析出component 中的变量 `cardlist`,因此最好还是用`(card, index) in cardlist`这样的写法
+```html
+<div v-for="(card, index) of cardlist">{{card}}</div>
+```
--- src/example.ts
+++ src/example.ts
@@ -2,13 +2,12 @@ import * as fs from 'fs';
 import * as path from 'path';
 import { parseTemplate } from './parse/template';
 import { ScriptProcessor } from './parse/script';
-// const template = fs.readFileSync(path.resolve(__dirname, './template.test/movie.test.vue'));
-const template = fs.readFileSync(path.resolve(__dirname, './parse/__test__/eletemplate.test.vue'));
+const template = fs.readFileSync(path.resolve(__dirname, './template.test/movie.test.vue'));
+// const template = fs.readFileSync(path.resolve(__dirname, './parse/__test__/eletemplate.test.vue'));
 
 const file = template.toString();
 console.time('template');
 const tokenList = parseTemplate(file);
-console.log(tokenList)
 console.timeEnd('template');
 console.time('script');
 const sp = new ScriptProcessor(tokenList, file);
